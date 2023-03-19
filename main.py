a99142c9e574fc8f1fc0b6c5b73550c0c3e7140f
victorlcs <victor.loh95@gmail.com>
2023-03-19 16:48:39 +0800

    set driver option to make browsing easier

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-print("Hello World")
+from selenium import webdriver
 
-def cal(x,y):
-  return x*y
+def get_driver():
+    #set options to make browsing easier
+    options = webdriver.ChromeOptions()
+    # to diable the info bar pop up when driver is runnung
+    options.add_argument("disable-infobars")
+    # to start the browser in maximized
+    options.add_argument("start-maximized")
+    # to avoid particular issue when you interact with a browser on a Linux
+    options.add_argument("disable-dev-shm-usage")
+    # to add sandboxing disbaled, to have greater privileges that the script is going to access and scrape
+    options.add_argument("no-sandbox")
+    # to avoid detection from browser because some browser dont like script so we enable our script to enable those browser
+    options.add_experimental_option("excludeSwitches",["enable-automation"])
+    options.add_argument("disable-blink-features=AUtomationControlled")
 
-print(cal(2,3))
+    driver = webdriver.Chrome(options)
