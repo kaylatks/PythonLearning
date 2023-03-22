c23ef7277c4894486a478ee47eba564615dc6fb3
victorlcs <victor.loh95@gmail.com>
2023-03-22 19:02:03 +0800

    Scrap Simple text from a webpage

--- main.py
+++ main.py
@@ -16,3 +16,12 @@ def get_driver():
     options.add_argument("disable-blink-features=AUtomationControlled")
 
     driver = webdriver.Chrome(options)
+    driver.get("http://automated.pythonanywhere.com/")
+    return driver
+
+def main():
+    driver = get_driver()
+    element = driver.find_element_by_xpath("/html/body/div[1]/div/h1[1]")
+    return element
+
+print(main())
