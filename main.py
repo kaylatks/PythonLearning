70c6c9847936891e8e291e76704972afbb6727e1
victorlcs <victor.loh95@gmail.com>
2023-04-05 19:53:42 +0800

    Write the result into text file for every 2 second

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 import time
 from selenium.webdriver.common.keys import Keys
+from datetime import datetime as dt
+
+
+
 #set the chrome using the latest chrome version
 service= Service('C:\\Users\\user\\Downloads\\chromedriver.exe')
 
@@ -30,6 +34,12 @@ def clean_text(text):
     output = float(text.split(": ")[1])
     return output
 
+def WriteFile(text):
+    """"Write input text into a text file"""""
+    filename = f"{dt.now().strftime('%Y-%m-%d.%H-%M-%S')}.txt"
+    with open(filename,'w') as file:
+        file.write(text)
+
 def main():
     driver = get_driver()
     driver.find_element(by="id",value="id_username").send_keys("automated")
@@ -38,9 +48,13 @@ def main():
     driver.find_element(by="id",value="id_password").send_keys("automatedautomated" + Keys.RETURN)
     time.sleep(2)
     driver.find_element(by="xpath",value="/html/body/nav/div/a").click()
-    time.sleep(2)
-    timer = driver.find_element(by="xpath",value="/html/body/div[1]/div/h1[2]").text
-    print(clean_text(timer))
+    while True:
+        time.sleep(2)
+        timer = driver.find_element(by="xpath",value="/html/body/div[1]/div/h1[2]").text
+        text = str(clean_text(timer))
+        WriteFile(text)
     # print(driver.current_url)
 
 print(main())
+
+#rm *.txt
